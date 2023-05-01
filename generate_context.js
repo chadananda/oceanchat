58f4ef173d261aecab507b2eeebbcfd991dcae91
Chad <chadananda@gmail.com>
2023-05-01 14:01:19 -0700

    updated generate_context script

--- generate_context.js
+++ generate_context.js
@@ -1,9 +1,5 @@
-// generate_context.js
-
-// dumps important information into a context.txt file for AIs to read in order to understand the project
-
-const fs = require('fs');
-const path = require('path');
+import fs from 'fs';
+import path from 'path';
 
 // Function to recursively scan a directory for files matching a pattern
 function scanDirectory(directory, pattern, fileList = []) {
@@ -15,7 +11,7 @@ function scanDirectory(directory, pattern, fileList = []) {
 
     if (stat.isDirectory()) {
       scanDirectory(filePath, pattern, fileList);
-    } else if (file.match(pattern)) {
+    } else if (pattern.test(file)) {
       fileList.push(filePath);
     }
   }
@@ -25,20 +21,22 @@ function scanDirectory(directory, pattern, fileList = []) {
 
 // Generate context
 function generateContext() {
-  const contextFilePath = path.join(__dirname, 'context.txt');
-  const fileTree = scanDirectory(__dirname, '*.config.js');
+  const scriptUrl = new URL(import.meta.url);
+  const projectDir = path.dirname(scriptUrl.pathname);
+  const contextFilePath = path.join(projectDir, 'context.txt');
+  const fileTree = scanDirectory(projectDir, /\.config\.js$/, []);
 
   fs.writeFileSync(contextFilePath, '--- FILE TREE ---\n');
-  fs.appendFileSync(contextFilePath, fs.readFileSync(path.join(__dirname, 'tree.txt')));
+  fs.appendFileSync(contextFilePath, fs.readFileSync(path.join(projectDir, 'tree.txt')));
 
   fs.appendFileSync(contextFilePath, '\n\n--- PACKAGE.JSON ---\n');
-  fs.appendFileSync(contextFilePath, fs.readFileSync(path.join(__dirname, 'package.json')));
+  fs.appendFileSync(contextFilePath, fs.readFileSync(path.join(projectDir, 'package.json')));
 
   fs.appendFileSync(contextFilePath, '\n\n--- PROJECT_DESCRIPTION.MD ---\n');
-  fs.appendFileSync(contextFilePath, fs.readFileSync(path.join(__dirname, 'project_description.md')));
+  fs.appendFileSync(contextFilePath, fs.readFileSync(path.join(projectDir, 'project_description.md')));
 
   fs.appendFileSync(contextFilePath, '\n\n--- PROJECT_STATUS.MD ---\n');
-  fs.appendFileSync(contextFilePath, fs.readFileSync(path.join(__dirname, 'project_status.md')));
+  fs.appendFileSync(contextFilePath, fs.readFileSync(path.join(projectDir, 'project_status.md')));
 
   for (const configFile of fileTree) {
     fs.appendFileSync(contextFilePath, `\n\n--- ${path.basename(configFile).toUpperCase()} ---\n`);
